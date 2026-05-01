074ec62963284f548e6120085efdae080a17f1ee
Mostnito <Tapanawatsunanta@gmail.com>
2026-05-02 00:27:43 +0700

    some frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route,Router,Routes } from 'react-router-dom';
 
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 function App(){
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
