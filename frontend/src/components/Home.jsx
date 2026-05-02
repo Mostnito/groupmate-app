86dbea0424bb24a121797815fd709747c67939e5
Mostnito <Tapanawatsunanta@gmail.com>
2026-05-02 16:12:57 +0700

    landing page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,18 @@
+import './Home.css';
 function Home() {
     return ( 
-        <div>
-            <h1>Welcome to GroupMate!</h1>
-            <p>Your ultimate group management tool.</p>
+        <div className="home-container">
+            <div className="header">
+                <h1>GroupMate</h1>
+                <button className='header-btn'>เริ่มต้นใช้งาน</button>
+            </div>
+            <div className="banner">
+                <div className="banner-content">
+                    <h1>GroupMate แอปพลิเคชั่นการทำงานกลุ่ม</h1>
+                    <p>การสร้างกลุ่มสำหรับหรือทีมงาน GroupMate มีเครื่องมือที่ช่วยให้คุณสามารถเชิญสมาชิก จัดการงาน และสื่อสารกันได้อย่างมีประสิทธิภาพ มาเริ่มต้นใช้งาน GroupMate กันเถอะ!</p>
+                    <button>เริ่มต้นใช้งาน</button>
+                </div>
+            </div>
         </div>
      );
 }
